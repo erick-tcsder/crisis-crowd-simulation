6dfcfae9b231c4a23baf5b53d0cb9166f4739110
dbyta <dbyf.rguez@gmail.com>
2022-12-08 20:18:41 -0500

    feat: modifying agent

--- Agents/agents.py
+++ Agents/agents.py
@@ -1,19 +1,34 @@
-from utils.position import Position
-from actions import Actions
-
-
-class Personality:
-    def __init__(self, courage: int, kindness: int, ):
-        self.courage = courage
-        self.kindness = kindness
-
 class Agent:
-    def __init__(self, name, position : Position, velocity: float, actions: list(Actions),
-                 state: int, personality: Personality, behavior: BehaviorFunctions):
+    def __init__(self, name, location, graph):
         self.name = name
-        self.position = position
-        self.velocity = velocity
-        self.actions = actions
-        self.state = state
-        self.personality = personality
-        self.behavior = behavior
+        self.location = location
+        self.out = False  # Indica si el agente ha logrado salir del edificio
+        self.graph = graph  # Grafo que representa las posibles rutas en el edificio
+
+    def move_random(self, destinations):
+        # destinations es una lista de posibles destinos al que el agente puede
+        # moverse, y se elige uno de forma aleatoria
+        self.location = random.choice(destinations)
+
+        # Si el agente se mueve a la ubicación de la salida del edificio, se marca
+        # como habiendo logrado salir
+        if self.location == "Exit":
+            self.out = True
+
+    def move_a_star(self, destinations, heuristic):
+        # destinations es una lista de posibles destinos al que el agente puede
+        # moverse, y se utiliza el algoritmo A* para seleccionar el camino más
+        # cercano a la salida (indicada por la función heurística)
+        path = A_star(self.graph, self.location, "Exit", heuristic)
+        self.location = path[1]  # Se mueve al segundo nodo en el camino encontrado
+
+        # Si el agente se mueve a la ubicación de la salida del edificio, se marca
+        # como habiendo logrado salir
+        if self.location == "Exit":
+            self.out = True
+
+    def interact(self, other):
+        if self.location == other.location:
+        # Si ambos agentes están en la misma ubicación, pueden interactuar
+        # de acuerdo a una lógica específica, como, por ejemplo, ayudarse
+        # mutuamente a evacuar el edificio o compartir información
