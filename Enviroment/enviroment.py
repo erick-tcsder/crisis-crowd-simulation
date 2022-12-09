a59e8d9fe912e25ad8869178aaeb4b42e80713aa
dbyta <dbyf.rguez@gmail.com>
2022-12-08 20:21:56 -0500

    feat: modifying enviroment

--- Enviroment/enviroment.py
+++ Enviroment/enviroment.py
@@ -1,28 +1,18 @@
-from utils.position import *
-from objEntities import *
-from abc import ABC
-
-class Floor:
-    def __init__(self, n_windows: int, n_doors: int, n_evacuation_signals: int, floor_number: int, n_stairs,
-                 objEntities: list(ObjEntity), length: int, width: int, n_agents):
-        self.n_windows = n_windows
-        self.n_doors = n_doors
-        self.n_evacuation_signals = n_evacuation_signals
-        self.n_stairs = n_stairs
-        self.n_agent = n_agents
-        self.id = floor_number
-        self.objEntities = objEntities
-        self.length = length
-        self.width = width
-        self.structure = objEntities[length,width]
-    
-    def initializeParameters(self):
-        pass
-
 class Building:
-    def __init__(self, n_floors: int, n_doors: int):
-        self.n_floors = n_floors
-        self.n_doors = n_doors        
-    
-    def initializeParameters(self):
-        pass
+  def __init__(self, agents, graph):
+    self.agents = agents  # Lista de agentes en el edificio
+    self.graph = graph  # Grafo que representa las posibles rutas en el edificio
+    self.exited_agents = []  # Lista de agentes que han logrado salir del edificio
+
+  def simulate(self, steps, move_random=False, move_a_star=False, heuristic=None):
+    # steps es el número de pasos que se van a simular
+    for i in range(steps):
+      # Se recorren todos los agentes y se les permite moverse o interactuar
+      # de acuerdo a los argumentos proporcionados
+      for agent in self.agents:
+        if move_random:
+        # Si move_random es True, se permite que el agente se mueva
+            pass
+        if move_a_star:
+        # Si move_a_star es True, se permite que el agente se mueva
+            pass
--- utils/position.py
+++
@@ -1,6 +0,0 @@
-class Position:
-    def __init__(self, x, y, length, width):
-        self.x = x
-        self.y = y
-        self.length = length
-        self.width = width
