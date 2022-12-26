ff00bede05fc6fbdaf61e4fe633d3ae8568bed42
Erick A. Fonseca Perez <erickfons00@gmail.com>
2022-12-25 20:49:40 -0500

    feat: initializing servers

---
+++ backend/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(debug=True)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/')
+def main():
+  return 'Hello World!!!'
