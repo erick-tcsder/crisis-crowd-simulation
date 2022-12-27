53f546efb1db8bc8b25cde98ac34261443473451
deborahfam <deborahfamadas@gmail.com>
2022-12-27 09:06:09 -0500

    adding: minimal changes

--- backend/Agents/agents.py
+++ backend/Agents/agents.py
@@ -1,142 +1,133 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from numpy.linalg import norm
 import matplotlib.animation as animation
 from shapely.geometry import Point, Polygon
+from numpy.linalg import norm
 
-# Class for pedestrian
+
 # Class for pedestrian
 class Pedestrian:
     def __init__(self, p: np.array, m: float, t: float, A: float, B: float, j: float, r: float,
                  k: float, K: float, v_desired: float, boundary_min: np.array, boundary_max: np.array):
-        self.p = p                  # posición
-        self.m = m                  # masa
-        self.v = np.zeros_like(p)   # velocidad
-        self.t = t                  # tiempo de actualización
-        self.A = A                  # intensidad de la fuerza repulsiva entre peatones
-        self.B = B                  # factor de cambio de la fuerza repulsiva respecto la distancia
-        self.j = j                  # la distancia mínima que el peatón debe mantener con respecto a otros
-        self.r = r                  # radio del peatón            
-        self.k = k                  # constante de fuerza corporal
-        self.K = K                  # constante de fricción
+        self.p = p                      # position
+        self.m = m                      # mass
+        self.v = np.zeros_like(p)       # velocity
+        self.t = t                      # update time
+        self.A = A                      # intensity of repulsive force between pedestrians
+        self.B = B                      # change factor of repulsive force with respect to distance
+        self.j = j                      # minimum distance that the pedestrian must maintain with respect to others
+        self.r = r                      # pedestrian radius
+        self.k = k                      # body force constant
+        self.K = K                      # friction constant
 
-        self.v_desired = v_desired          # velocidad deseada
-        self.boundary_min = boundary_min    # límite inferior del espacio en el que se mueve el peatón
-        self.boundary_max = boundary_max    # límite superior del espacio en el que se mueve el peatón  
-    
-    def wall_force(self, walls: ['Wall']):
-         # Inicializamos la aceleración.
-        fiW = np.zeros_like(self.p)
+        self.v_desired = v_desired          # desired speed
+        self.boundary_min = boundary_min    # lower limit of the space in which the pedestrian moves
+        self.boundary_max = boundary_max    # upper limit of the space in which the pedestrian moves
 
-        # Añadimos la fuerza de las interacciones con las paredes.
-        for wall in walls:
-            # Calculamos la distancia de este peatón al otro.
-            dif = self.wall_difference(wall)           # vector diferencia
-            diW = norm(dif)                            # norma del vector diferencia (distancia)
-
-            # Si el peatón está demasiado cerca de la pared.
-            if diW < self.j:
-                niW = dif / diW             # vector diferencia normalizado
-                r = self.r                  # radio del peatón
-                tiW = np.flip(niW)          # dirección tangencial
-                tiW[0] = -tiW[0]
-                dv = np.cross(self.v, tiW)  # velocidad tangencial
-
-                # Fuerza de repulsión entre peatones
-                f_repulsive = self.A * np.exp((r - diW) / self.B) * niW
-
-                # Fuerza corporal que el peatón ejerce a la pared.
-                # Si la distancia es menor que la suma de los radios, entonces los peatones se tocan.
-                # Solo en estos casos interviene la fuerza corporal entre peatones.
-                f_body = (self.k * (r - diW) if diW > r else 0) * niW
-
-                # Si la distancia es menor que la suma de los radios, entonces los peatones se tocan.
-                # Solo en estos casos interviene la fuerza corporal entre peatones.
-                f_friction = (self.K * (r - diW) * dv if diW > r else 0) * tiW
-
-                # Sumamos todas las fuerzas para calcular la fuerza total.
-                fiW += f_repulsive + f_body + f_friction
-
-        # Devolvemos la fuerza de repulsión total entre el peatón y las paredes.
-        return fiW
-    
-    # Calcula la distancia de un peatón a una pared.
-    def wall_difference(self, wall: 'Wall'):
-        p = self.p    # posicion del peatón
-        p1 = wall.p1  # coordenada de inicio de la pared
-        p2 = wall.p2  # coordenada de final de la pared
-
-        # Comprobamos que el punto no corresponda a los extremos del segmento.
-        if all(p1 == p) or all(p2 == p):
-            return np.zeros_like(p)
-
-        # Calculamos el angulo entre AB y AP, si es mayor de 90 grados retornamos la distancia enre p1 y p
-        elif np.arccos(np.dot((p - p1) / norm(p - p1), (p2 - p1) / norm(p2 - p1))) > np.pi / 2:
-            return p - p1
-
-        # Calculamos el angulo entre AB y BP, si es mayor de 90 grados retornamos la distancia enre p2 y p.
-        elif np.arccos(np.dot((p - p2) / norm(p - p2), (p1 - p2) / norm(p1 - p2))) > np.pi / 2:
-            return p - p2
-
-        # Como ambos angulos son menores o iguales a 90 sabemos que podemos hacer una proyección ortogonal del punto.
-        # Considere el triangulo rectangulo que tiene la proyección ortogonal del peatón sobre la pared (p_o)
-        # como primer cateto, el segmento desde el primer extremo de la pared hasta el punto de corte (p1_o)
-        # como segundo cateto, y el segmento entre el peatón y el primer extremo de la pared como hipotenusa.
-        p_o = norm(np.cross(p2 - p1, p1 - p)) / norm(p2 - p1)  # Calculamos la longitud de la hipotenusa.
-        p_p1 = norm(p - p1)                                    # Calculamos la longitud del primer cateto.
-        p1_o = np.sqrt(p_p1**2 - p_o**2)                       # Calculamos la longitud del segundo cateto.
-
-        # Determinamos el punto o calculando explicitamente el vector ortogonal.
-        # Determinamos el vector p1_o normalizando p1_p2 y multiplicandolo por la norma de p1_o.
-        o = p1 + (p2 - p1) / norm(p2 - p1) * p1_o
-        # Devolvemos el vector ortogonal.
-        return p - o
-    
     # Función para calcular la fuerza de repulsión respecto al resto de peatones.
-    def repulsion_force(self,  pedestrians: ['Pedestrian']):
-        # Inicializamos la aceleración.
+    def repulsion_force(self, pedestrians: ['Pedestrian']):
+        # Initialize acceleration.
         fij = np.zeros_like(self.p)
 
-        # Añadimos la fuerza de las interacciones con otros peatones.
+        # Add the force of interactions with other pedestrians.
         for pedestrian in pedestrians:
-            # Ignoramos a este peatón (no tiene sentido considerar las interacciones consigo mismo).
+            # Ignore this pedestrian (it doesn't make sense to consider self-interactions).
             if pedestrian == self:
                 continue
 
-            # Calculamos la distancia de este peatón al otro.
-            dif = pedestrian.p - self.p                # vector diferencia
-            dij = norm(dif)                            # norma del vector diferencia (distancia)
+            # Calculate the distance from this pedestrian to the other.
+            dif = pedestrian.p - self.p                # difference vector
+            dij = norm(dif)                            # norm of difference vector (distance)
 
-            # Si el peatón está demasiado cerca de la pared.
+            # If the pedestrian is too close to the wall.
             if dij < self.j:
-                nij = dif / dij                            # vector diferencia normalizado
-                rij = self.r + pedestrian.r                # suma de los radios de ambos peatones
-                tij = np.flip(nij)                         # dirección tangencial
+                nij = dif / dij                            # normalized difference vector
+                rij = self.r + pedestrian.r                # sum of the radii of both pedestrians
+                tij = np.flip(nij)                         # tangential direction
                 tij[0] = -tij[0]
-                dv = np.cross(pedestrian.v - self.v, tij)  # diferencia de velocidad tangencial
+                dv = np.cross(pedestrian.v - self.v, tij)  # tangential velocity difference
 
-                # Fuerza de repulsión entre peatones
+                # Repulsion force between pedestrians
                 f_repulsive = self.A * np.exp((rij - dij) / self.B) * nij
 
-                # Fuerza corporal entre peatones.
-                # Si la distancia es menor que la suma de los radios, entonces los peatones se tocan.
-                # Solo en estos casos interviene la fuerza corporal entre peatones.
+                # Body force between pedestrians.
                 f_body = (self.k * (rij - dij) if dij > rij else 0) * nij
 
-                # Si la distancia es menor que la suma de los radios, entonces los peatones se tocan.
-                # Solo en estos casos interviene la fuerza corporal entre peatones.
                 f_friction = (self.K * (rij - dij) * dv if dij > rij else 0) * tij
 
-                # Sumamos todas las fuerzas para calcular la fuerza total.
+                # Sum all forces to calculate the total force.
                 fij += f_repulsive + f_body + f_friction
 
-        # Devolvemos la fuerza de repulsión total entre los peatones.
+        # Return the total repulsion force between pedestrians.
         return fij
-    
-    # Función para actualizar la velocidad del peatón.
+
+    # Calculates the distance of a pedestrian to a wall.
+    def wall_difference(self, wall: 'Wall'):
+        p = self.p    # pedestrian position
+        p1 = wall.p1  # wall start coordinate
+        p2 = wall.p2  # wall end coordinate
+
+        # Check that the point does not correspond to the ends of the segment.
+        if all(p1 == p) or all(p2 == p):
+            return np.zeros_like(p)
+
+        # Calculate the angle between AB and AP, if it is greater than 90 degrees return the distance between p1 and p
+        elif np.arccos(np.dot((p - p1) / norm(p - p1), (p2 - p1) / norm(p2 - p1))) > np.pi / 2:
+            return p - p1
+
+        # Calculate the angle between AB and BP, if it is greater than 90 degrees return the distance between p2 and p.
+        elif np.arccos(np.dot((p - p2) / norm(p - p2), (p1 - p2) / norm(p1 - p2))) > np.pi / 2:
+            return p - p2
+
+        p_o = norm(np.cross(p2 - p1, p1 - p)) / norm(p2 - p1)  # Calculate the length of the hypotenuse.
+        p_p1 = norm(p - p1)                                    # Calculate the length of the first leg.
+        p1_o = np.sqrt(p_p1**2 - p_o**2)                       # Calculate the length of the second leg.
+
+        # Determine the point o by explicitly calculating the orthogonal vector.
+        o = p1 + (p2 - p1) / norm(p2 - p1) * p1_o
+        
+        # Return the orthogonal vector.
+        return p - o
+
+    #Function to calculate the repulsion force with respect to the walls.
+    def wall_force(self, walls: ['Wall']):
+        # Initialize acceleration.
+        fiW = np.zeros_like(self.p)
+
+        # Add the force of interactions with the walls.
+        for wall in walls:
+            # Calculate the distance from this pedestrian to the wall.
+            dif = self.wall_difference(wall)           # difference vector
+            diW = norm(dif)                            # norm of difference vector (distance)
+
+            # If the pedestrian is too close to the wall.
+            if diW < self.j:
+                niW = dif / diW             # normalized difference vector
+                r = self.r                  # pedestrian radius
+                tiW = np.flip(niW)          # tangential direction
+                tiW[0] = -tiW[0]
+                dv = np.cross(self.v, tiW)  # tangential velocity
+
+                # Repulsion force between pedestrian and wall
+                f_repulsive = self.A * np.exp((r - diW) / self.B) * niW
+
+                # Body force that the pedestrian exerts on the wall.
+                f_body = (self.k * (r - diW) if diW > r else 0) * niW
+
+                # Friction force between pedestrian and wall.
+                f_friction = (self.K * (r - diW) * dv if diW > r else 0) * tiW
+
+                # Sum all forces to calculate the total force.
+                fiW += f_repulsive + f_body + f_friction
+
+        # Return the total repulsion force between pedestrian and walls.
+        return fiW
+
+
+    # Function to update the pedestrian speed.
     def update_velocity(self, dt: float, pedestrians: ['Pedestrian'], walls: ['Wall'], exits: ['Exit']):
-        fij = self.repulsion_force(pedestrians)  # Calculamos la fuerza de repulsión de este peatón con los demás.
-        fiW = self.wall_force(walls)             # Calculamos la fuerza de repulsión de este peatón con las paredes.
+        fij = self.repulsion_force(pedestrians)  # We calculate the force of repulsion of this pedestrian with the others.
+        fiW = self.wall_force(walls)             # We calculate the repulsive force of this pedestrian with the walls.
 
         # Add forces from interactions with exits
         found_exit = False
@@ -167,52 +158,54 @@ class Pedestrian:
         if v_norm > self.v_desired:
             self.v *= self.v_desired / v_norm
 
-def distance_to_wall(pedestrian, wall):
-    # Create a Shapely Point object from pedestrian position
-    point = Point(pedestrian.r)
-    # Create a Shapely Polygon object from wall vertices
-    polygon = Polygon(wall.vertices)
-    # Calculate minimum distance between point and polygon
-    distance = point.distance(polygon)
-    return distance
+    # Function to update pedestrian speed
+    def update_position(self):
+        # Updates the position with respect to speed and elapsed time (p = v * t)
+        self.p += self.v * t
 
-# Function to update position of pedestrian
-def update_position(pedestrian, dt):
-            
-    # Update position according to velocity
-    pedestrian.r += pedestrian.v * dt
+        # Verifica si el peatón se ha pasado de los bordes
+        for i in range(2):
+            if self.p[i] < self.boundary_min[i]:
+                self.p[i] = self.boundary_min[i]
+                self.v[i] = -self.v[i]
+            elif self.p[i] > self.boundary_max[i]:
+                self.p[i] = self.boundary_max[i]
+                self.v[i] = -self.v[i]
 
-    # Check if pedestrian has crossed boundary
-    for i in range(2):
-        if pedestrian.r[i] < pedestrian.boundary_min[i]:
-            pedestrian.r[i] = pedestrian.boundary_min[i]
-            
-            pedestrian.v[i] = -pedestrian.v[i]
-        elif pedestrian.r[i] > pedestrian.boundary_max[i]:
-            pedestrian.r[i] = pedestrian.boundary_max[i]
-            pedestrian.v[i] = -pedestrian.v[i]
+
+# def distance_to_wall(pedestrian, wall):
+#     # Create A Shapely Point object from pedestrian position
+#     point = Point(pedestrian.p)
+#     # Create A Shapely Polygon object from wall vertices
+#     polygon = Polygon(wall.vertices)
+#     # Calculate minimum distance between point and polygon
+#     distance = point.distance(polygon)
+#     return distance
 
 class Wall:
     def __init__(self, vertices):
         self.vertices = vertices
         # Compute center of mass of wall
-        self.r = np.mean(vertices, axis=0)
+        self.p = np.mean(vertices, axis=0)
+        self.p1 = np.array([1, 2])
+        self.p2 = np.array([3, 4])
 
     def draw(self):
         plt.plot(self.vertices[:, 0], self.vertices[:, 1], 'k', lw=2)
+
 
 class Exit:
     def __init__(self, vertices):
         self.vertices = vertices
         # Compute center of mass of wall
-        self.r = np.mean(vertices, axis=0)
+        self.p = np.mean(vertices, axis=0)
 
     def draw(self):
         plt.plot(self.vertices[:, 0], self.vertices[:, 1], 'r', lw=2)
 
 
 # Set up parameters of simulation
-dt = 0.7
+dt = 1 # Tiempo transcurrido entre pasos de la simulación
 n_steps = 300
 
 # Set number of pedestrians
@@ -226,27 +219,29 @@ boundary_max = np.array([10, 10])
 m = 80
 
 # Set up parameters of interaction forces
-T = 0.5
+t = 1
 A = 1
 B = 0.5
 k = 1
+K = 1
+r = 0.5
 
 # Set up desired velocity of pedestrians
 v_desired = 5
 
 # Set up minimum distance between pedestrians
-min_distance = 0.5
+j = 0.5
 
 # Set up initial positions and velocities of pedestrians
 pedestrians = []
 for i in range(n_pedestrians):
-    r = np.random.uniform(boundary_min, boundary_max, size=2)
+    p = np.random.uniform(boundary_min, boundary_max, size=2)
     v = np.zeros(2)
-    pedestrians.append(Pedestrian(r, m, T, A, B, v_desired, boundary_min, boundary_max, min_distance,k))
+    pedestrians.append(Pedestrian(p, m, t, A, B, j, r, k, K, v_desired, boundary_min, boundary_max))
 
 # Set up walls
-walls = []
-vertices = np.array([[0, 0], [10, 0], [10, 10], [0, 10]])
+walls = [] # Rectangulos interiores
+vertices = np.array([[0, 0], [10, 0], [10, 10], [0, 10]]) # Vertices del rectangulo
 walls.append(Wall(vertices))
 
 # Set up exits
@@ -264,7 +259,7 @@ ax.set_ylim((boundary_min[1], boundary_max[1]))
 # Initialize list of scatter plots for pedestrians
 scatters = []
 for pedestrian in pedestrians:
-    scatters.append(ax.scatter([pedestrian.r[0]], [pedestrian.r[1]], color='k'))
+    scatters.append(ax.scatter([pedestrian.p[0]], [pedestrian.p[1]], color='k'))
 
 # Set up plots for walls and exits
 lines_walls = []
@@ -274,15 +269,17 @@ lines_exits = []
 for exit in exits:
     lines_exits.append(ax.plot(exit.vertices[:, 0], exit.vertices[:, 1], color='r'))
 
+
 # Set up animation function
 def animate(i):
     # Update positions and velocities of pedestrians
     for pedestrian in pedestrians:
         pedestrian.update_velocity(dt, pedestrians, walls, exits)
-        update_position(pedestrian, dt)
+        pedestrian.update_position()
     # Update scatter plots for pedestrians
     for i, pedestrian in enumerate(pedestrians):
-        scatters[i].set_offsets(pedestrian.r)
+        scatters[i].set_offsets(pedestrian.p)
+
 
 # Set up animation
 anim = animation.FuncAnimation(fig, animate, frames=range(n_steps), interval=200)
@@ -290,5 +287,5 @@ anim = animation.FuncAnimation(fig, animate, frames=range(n_steps), interval=200
 # Show plot
 plt.show()
 
-#anim.save('animation.mp4')
-#anim.show()
+# anim.save('animation.mp4')
+# anim.show()
