434adc45f4ae6b9aca5148fb34e224ab2fe701d3
Gabriel Herrod <gabrielhdezrodriguez@gmail.com>
2022-12-28 08:51:18 -0500

    feat: navmesh approximate point calculation and test notebook

--- backend/navmesh.py
+++ backend/navmesh.py
@@ -2,7 +2,7 @@ from typing import Dict, List, Set, Tuple
 from shapely import Point, Polygon, MultiPolygon, LineString
 from shapely.ops import triangulate
 from dataclasses import dataclass
-import numpy as np
+import time
 
 ADJACENT_MATRIX = Dict[Polygon, Set[Polygon]]
 
@@ -60,3 +60,82 @@ def build_navmesh(
                 adj[l[1]].add(l[0])
 
     return Navmesh(multi, adj)
+
+
+def approx_navmesh(navmesh: Navmesh, p: Point, allow_borders=True) -> Tuple[Point, bool, bool]:
+    """
+    Given a navmesh and a point gives the closest point o the navmesh to the given point.
+    If `allow_borders` is `True` it can give points that are in the borders of the navmesh.
+    
+    Returns a tuple (`p`,`in`,`center`) where:
+    - `p` is the point of the navmesh.
+    - `in` is a boolean that is `True` if the point given is at least inside the geometry
+    of the navmesh, `False` otherwise.
+    - `center` is a boolean that is `True` if the point in the return is the center of
+    a triangle in the navmesh, `False` otherwise. Must be always `True` if `allow_borders`
+    is `False`.
+    """
+    tris = list(navmesh.polygon.geoms)
+
+    start = time.time()
+
+    # Find the tri with the minimum distance to the given point
+    p_tri = (tris[0], p.distance(tris[0]))
+    for t in tris[1:]:
+        d = p.distance(t)
+        if d < p_tri[1]:
+            p_tri = (t, d)
+
+    print(f"Finished tri find in {time.time()-start:.3f}s")
+
+    # If the distance is 0 or below is because is inside
+    _in = p_tri[1] <= .0
+
+    # Get the centroid of the closest triangle
+    center = p_tri[0].centroid
+
+    if allow_borders:
+        # From all the points in the border pick the closest
+        tri_points: List[Point] = [Point(p) for p in p_tri[0].boundary.coords]
+        f_p = (tri_points[0], p.distance(tri_points[0]))
+        for tp in tri_points[1:]:
+            d = p.distance(tp)
+            if d < f_p[1]:
+                f_p = (tp, d)
+
+        # Compare the closest border to the centroid and pick closest
+        if p.distance(center) < f_p[1]:
+            return (center, _in, True)  # Centroid is closer
+        else:
+            return (f_p[0], _in, False)  # The border was closer
+    else:
+        #Just return the centroid if the borders are not valid
+        return (center, _in, True)
+
+
+def a_star(navmesh: Navmesh, start: Point, end: Point) -> Tuple[List[Point], Point, Point]:
+    # First thing is to find the closest point in the navmesh to use as start and end
+
+    tris = navmesh.polygon.geoms
+
+    # Get the closest tri to start point
+    start_tri = (tris[0], start.distance(tris[0]))
+    for t in tris[1:]:
+        d = start.distance(t)
+        if d < start_tri[1]:
+            start_tri = (t, d)
+
+    # Get the closest tri to end point
+    end_tri = (tris[0], end.distance(tris[0]))
+    for t in tris[1:]:
+        d = end.distance(t)
+        if d < end_tri[1]:
+            end_tri = (t, d)
+
+    # Get the closest point of each tri (Center or vertices)
+    st_points: List[Point] = list(start_tri[0].coords)+[start_tri[0].centroid]
+    end_points: List[Point] = list(end_tri[0].coords)+[end_tri[0].centroid]
+
+    start_pos = (st_points[0], start)
+    for p in st_points:
+        pass
