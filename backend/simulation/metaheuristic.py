0427827438a5f53e5b5f7fd99a469c4ad7f23114
Gabriel Herrod <gabrielhdezrodriguez@gmail.com>
2023-01-07 02:18:50 -0500

    fix: final fixes

--- backend/simulation/metaheuristic.py
+++ backend/simulation/metaheuristic.py
@@ -77,6 +77,6 @@ def vulnerability_data(map: Blueprint, damage_radius: float):
 
     new_gen = gen_engine.next_gen(first_gen)
     while True:
-        limit = min(len(new_gen), 10)
+        limit = min(len(new_gen), 3)
         yield (gen_engine.generation, [(c.value.x*map.width, c.value.y*map.height) for c in new_gen[:limit]])
         new_gen = gen_engine.next_gen()
